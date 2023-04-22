dee0db1525c123f24b5fcba531ea4a7175ccd976
Mirlok3 <pherafixx@gmail.com>
2023-04-22 16:46:54 +0200

    Initial commit

---
+++ database/migrations/2023_04_22_143246_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('taker_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('taker_id')->references('id')->on('users');
+            $table->foreignId('department_id')->constrained();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('post_file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
